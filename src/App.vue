b855215a5d237d4d4659f64a1933e77eab6bf172
Dan Carlier <dan.carlier@gmail.com>
2024-01-06 19:48:56 +1100

    Add component layout

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,27 @@
-<template>My App</template>
+<template>
+  <TheHeader />
+  <div class="container">
+    <TheBalance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+import TheBalance from './components/TheBalance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheBalance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
+  }
+}
+</script>
