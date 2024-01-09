f26238cdde88fccaec76e52643158f8a462cbe36
Dan Carlier <dan.carlier@gmail.com>
2024-01-09 14:58:08 +1100

    Add new transaction with success toast

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <TheHeader />
   <div class="container">
-    <TheBalance :total="total"/>
+    <TheBalance :total="+total"/>
     <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions"/>
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted"/>
   </div>
 </template>
 
@@ -15,7 +15,11 @@ import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
 
+import { useToast } from 'vue-toastification'
+
 import { ref, computed } from 'vue'
+
+const toast = useToast()
 
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -19.99 },
@@ -48,4 +52,20 @@ const expenses = computed(() => {
     return acc + transaction.amount
   }, 0).toFixed(2)
 })
+
+// Add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  })
+
+  toast.success("Transaction added")
+}
+
+// Generate unique ID
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
 </script>
